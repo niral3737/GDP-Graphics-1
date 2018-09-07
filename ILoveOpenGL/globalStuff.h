2342cac612fc16a0423ceb0c69b8559e838212de
Niral <niral.eagle100@gmail.com>
2018-09-07 15:35:34 -0400

    adds shader manager

---
+++ ILoveOpenGL/globalStuff.h
@@ -0,0 +1,7 @@
+#ifndef _globalStuff_HG_
+#define _globalStuff_HG_
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#endif
--- ILoveOpenGL/original.cpp
+++ ILoveOpenGL/original.cpp
@@ -1,55 +1,43 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "globalStuff.h"
 #include "linmath.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
 
-#define PI 3.1415
+#include "cShaderManager.h"
+
+//stack based variable
+//cShaderManager manager;
+
+//Heap based variable
+cShaderManager* pShaderManager;
+
 static const struct
 {
 	float x, y;
 	float r, g, b;
-} vertices[] =
+} vertices[3] =
 {
-	//Orange Triangle
-	{0.25f, 0.0f, 1.0f, 0.6f, 0.0f},
-	{0.65f, -0.4f, 1.0f, 0.6f, 0.0f},
-	{0.65f, 0.4f, 1.0f, 0.6f, 0.0f},
-	//Pink Triangle
-	{-0.4f, 0.0f, 1.0f, 0.0f, 0.4f},
-	{0.4f, -0.8f, 1.0f, 0.6f, 0.8f},
-	{0.4f, 0.8f, 1.0f, 0.0f, 0.4f},
-	//Yellow Triangle
-	{-0.4f, 0.0f, 1.0f, 1.0f, 0.0f},
-	{0.0f, -0.4f, 1.0f, 1.0f, 0.0f},
-	{0.0f, 0.4f, 1.0f, 1.0f, 0.0f},
-	//Green Triangle
-	{0.1f, 0.0f, 0.0f, 1.0f, 0.0f},
-	{0.3f, -0.2f, 0.0f, 1.0f, 0.0f},
-	{0.3f, 0.2f, 0.0f, 1.0f, 0.0f},
-	//Black Square
-	{-0.15f, 0.05f, 0.0f, 0.0f, 0.0f},
-	{-0.05f, 0.05f, 0.0f, 0.0f, 0.0f},
-	{-0.05f, 0.15f, 0.0f, 0.0f, 0.0f},
-	{-0.15f, 0.15f, 0.0f, 0.0f, 0.0f}
-	
+	{ -0.6f, -0.4f, 1.f, 0.f, 0.f },
+	{  0.6f, -0.4f, 0.f, 1.f, 0.f },
+	{   0.f,  0.6f, 0.f, 0.f, 1.f }
 };
-static const char* vertex_shader_text =
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
-static const char* fragment_shader_text =
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
+//static const char* vertex_shader_text =
+//"uniform mat4 MVP;\n"
+//"attribute vec3 vCol;\n"
+//"attribute vec2 vPos;\n"
+//"varying vec3 color;\n"
+//"void main()\n"
+//"{\n"
+//"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+//"    color = vCol;\n"
+//"}\n";
+//static const char* fragment_shader_text =
+//"varying vec3 color;\n"
+//"void main()\n"
+//"{\n"
+//"    gl_FragColor = vec4(color, 1.0);\n"
+//"}\n";
 static void error_callback(int error, const char* description)
 {
 	fprintf(stderr, "Error: %s\n", description);
@@ -59,10 +47,10 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
-int main1(void)
+int main(void)
 {
 	GLFWwindow* window;
-	GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+	GLuint vertex_buffer, vertex_shader, fragment_shader/*, program*/;
 	GLint mvp_location, vpos_location, vcol_location;
 	glfwSetErrorCallback(error_callback);
 	if (!glfwInit())
@@ -79,20 +67,49 @@ int main1(void)
 	glfwMakeContextCurrent(window);
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 	glfwSwapInterval(1);
+
+	//Create Shader Manager....
+	pShaderManager = new cShaderManager();
+
+	cShaderManager::cShader vertexShader;
+	cShaderManager::cShader fragmentShader;
+
+	vertexShader.fileName = "vertex01.glsl";
+	vertexShader.shaderType = cShaderManager::cShader::VERTEX_SHADER;
+
+	fragmentShader.fileName = "fragment01.glsl";
+	fragmentShader.shaderType = cShaderManager::cShader::FRAGMENT_SHADER;
+
+	if (pShaderManager->createProgramFromFile("myShader", vertexShader, fragmentShader))
+	{
+		std::cout << "Works" << std::endl;
+	}
+	else
+	{
+		std::cout << "Compile error" << std::endl;
+		std::cout << pShaderManager->getLastError() << std::endl;
+	}
+
+
 	// NOTE: OpenGL error checks have been omitted for brevity
 	glGenBuffers(1, &vertex_buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-	glCompileShader(vertex_shader);
-	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-	glCompileShader(fragment_shader);
-	program = glCreateProgram();
-	glAttachShader(program, vertex_shader);
-	glAttachShader(program, fragment_shader);
-	glLinkProgram(program);
+
+
+	//vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	//glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+	//glCompileShader(vertex_shader);
+	//fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	//glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+	//glCompileShader(fragment_shader);
+	//program = glCreateProgram();
+	//glAttachShader(program, vertex_shader);
+	//glAttachShader(program, fragment_shader);
+	//glLinkProgram(program);
+
+	GLuint program = pShaderManager->getIDFromFriendlyName("myShader");
+
 	mvp_location = glGetUniformLocation(program, "MVP");
 	vpos_location = glGetAttribLocation(program, "vPos");
 	vcol_location = glGetAttribLocation(program, "vCol");
@@ -102,6 +119,9 @@ int main1(void)
 	glEnableVertexAttribArray(vcol_location);
 	glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
 		sizeof(float) * 5, (void*)(sizeof(float) * 2));
+
+
+
 	while (!glfwWindowShouldClose(window))
 	{
 		float ratio;
@@ -112,16 +132,19 @@ int main1(void)
 		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT);
 		mat4x4_identity(m);
-		//mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+		mat4x4_rotate_Z(m, m, (float)glfwGetTime());
 		mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
 		mat4x4_mul(mvp, p, m);
 		glUseProgram(program);
 		glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
-		glDrawArrays(GL_TRIANGLES, 0, 12);
-		glDrawArrays(GL_QUADS, 12, 4);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	//Delete Stuff
+	delete pShaderManager;
+
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	exit(EXIT_SUCCESS);
